7aa495ce19105b58daff593a7ba0abc94a7dd76e
staceymay <missstaceymay@gmail.com>
2016-11-14 20:09:00 -0800

    Added new and create page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,25 @@
 class ProductsController < ApplicationController
 
   def view_products
+    @view_products = Product.all
+  end
+
+
+  def index
     @products = Product.all
   end
 
-# def taxed
-#   @taxed = Product.price * 1.1
-# end
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @product.save
+  end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/view", to: "products#view_products"
+
+  get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  post "/products/create", to: "products#create"
+
+  get "/products/:id", to: "products#show"
   
 end
