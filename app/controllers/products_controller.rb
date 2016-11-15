cf3b823fc20c978c9c07889180229ab515cc6773
staceymay <missstaceymay@gmail.com>
2016-11-14 21:03:40 -0800

    Updated update form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,5 +23,8 @@ class ProductsController < ApplicationController
   end
 
   def update
+    product = Product.find_by(id: params[:id])
+    product.assign_attributes(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    product.save
   end
 end
