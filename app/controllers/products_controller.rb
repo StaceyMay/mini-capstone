8ffc3cdca3e7b1ac2359f468269af51d3de8a4df
staceymay <missstaceymay@gmail.com>
2016-11-15 20:59:26 -0800

    added faker to my database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,11 +26,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params[:id])
     product.assign_attributes(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
     product.save
+
+    redirect_to "/products/#{product.id}"
   end
 
   def destroy
     @product = Product.find_by(id: params[:id])
     @product.destroy
+
+    redirect_to "/products"
   end
 
 end
