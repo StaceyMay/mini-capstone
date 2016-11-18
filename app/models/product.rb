f1c517c34dea0dca3eb274a452e27717028c4aca
staceymay <missstaceymay@gmail.com>
2016-11-17 19:52:06 -0800

    Capstone Push

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,22 @@ class Product < ApplicationRecord
   def pretty_time
     return created_at.strftime("%b %e, %l:%M %p")
   end
+
+  def sale_message
+    if price.to_i <= 2
+      return "Discount Item"
+    else
+      return "On Sale!"
+    end
+  end
+
+  def tax
+    @tax = price.to_i * 0.09
+  end
+
+  def total
+    return price.to_i + @tax
+  end
+
 end
 
